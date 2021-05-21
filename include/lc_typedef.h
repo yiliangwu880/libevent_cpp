ef684eb96ad843bd26d1857d552ec79552d1b38d
yiliangwu880 <30390012@qq.com>
2021-05-21 12:15:25 +0800

    m

--- include/lc_typedef.h
+++ include/lc_typedef.h
@@ -5,11 +5,12 @@ brief:常用基本类型定义
 namespace lc //libevent cpp
 {
 
-	using uint16 = unsigned short ;
-	using uint32 = unsigned int ;
-	using uint64 = unsigned long long ;
-	using int64 = long long ;
-	using uint8 = unsigned char ;
-	using int32 = int ;
+	using uint16 = uint16_t;
+	using uint32 = uint32_t;
+	using uint64 = uint64_t;
+	using int64 = int64_t;
+	using uint8 = unsigned char;
+	using int32 = int;
+
 
 }
